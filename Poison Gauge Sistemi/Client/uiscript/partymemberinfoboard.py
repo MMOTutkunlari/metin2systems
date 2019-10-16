c656fd9ec860e97c360b915bed2631ea3093ca4b
MMOTutkunlari <gs.eray.55@hotmail.com>
2019-10-16 22:28:30 +0300

    +add uiscript code

---
+++ Poison Gauge Sistemi/Client/uiscript/partymemberinfoboard.py
@@ -0,0 +1,21 @@
+# UIScript/partymemberinfoboard.py veya locale/xx/ui/partymemberinfoboard.py
+# Casro2 Poison Guage Sistemi
+# MMO Tutkunları | www.mmotutkunlari.com | Whistle | Casro2 | www.casro2.com
+# Kodlandığı Tarih: 08.02.2019 - 16:44
+# Lütfen aşağıdaki adımları sırasıyla uygulayınız.
+
+# UIScript/partymemberinfoboard.py veya locale/xx/ui/partymemberinfoboard.py dosyasını açın ve en alta şu kodları ekleyin;
+window["children"] = window["children"] + (
+		{
+			"name" : "PoisonGauge",
+			"type" : "gauge",
+			"style" : ("not_pick",),
+
+			"x" : 22,
+			"y" : 17,
+			"width" : 84,
+			"color" : "lime",
+		},)
+
+# Bu dosyada yapacaklarımız bu kadardır. Bir sonraki dosyaya geçebilirsiniz.
+# Sabredin az kaldı. :)
